09ce78e2239d0c5f1ec2fb61b5cb5930d39cee6b
kamoresano <61633874+kamoresano@users.noreply.github.com>
2021-03-28 16:26:45 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
-function Welcome() {
-  alert("Disclamer");
+var visited = localStorage.getItem('visited');
+if (!visited) {
+  alert("Disclaimer: The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation.");
+  localStorage.setItem('visited', true);
 }
