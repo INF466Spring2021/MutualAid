cab545beae88f3f0674b09e5a6dbacb344f37f1c
sardinedcan <jnchan@albany.edu>
2021-03-28 18:57:20 -0400

    logo added

--- js.js
+++ js.js
@@ -1,3 +1,15 @@
 function Welcome() {
   alert("Disclaimer: The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation.");
 }
+
+var totopbutton = document.getElementById("totopbutton")
+var rootElement = document.documentElement
+
+function gototop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+totopbutton.addEventListener("click", gototop)
