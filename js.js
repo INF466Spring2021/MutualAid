5c0b5ece94d8663335e0ccae7cdba592b19c34ed
kamoresano <61633874+kamoresano@users.noreply.github.com>
2021-03-28 16:26:31 -0400

    update alert: only happens one time

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
-function Welcome() {
+var visited = localStorage.getItem('visited');
+if (!visited) {
   alert("Disclaimer: The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation.");
+  localStorage.setItem('visited', true);
 }
