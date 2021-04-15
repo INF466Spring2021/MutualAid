b86f152c092d818b85cfef3046dcd248b8e4d617
zachtay1021 <64520390+zachtay1021@users.noreply.github.com>
2021-04-15 14:08:59 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 var visited = localStorage.getItem('visited');
 if (!visited) {
-  alert("Disclaimer: The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation.");
+  alert("Disclaimer: The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation.");
   localStorage.setItem('visited', true);
 }
 
@@ -15,3 +15,32 @@ function gototop() {
   })
 }
 totopbutton.addEventListener("click", gototop)
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("collapsibleActive");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+$(document).on('click', '.collapsible', function() {
+    $(this).children().toggleClass('ui-icon-triangle-1-e ui-icon-triangle-1-s');
+            })
