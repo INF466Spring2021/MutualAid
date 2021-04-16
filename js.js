0e8c8e245d6fbd00e162ae16bc66b1116da244df
kamoresano <61633874+kamoresano@users.noreply.github.com>
2021-04-16 09:37:25 -0400

    changed alert from local to session storage

--- js.js
+++ js.js
@@ -19,10 +19,10 @@ function myFunction() {
 }
 
 /* Alert on visit */
-var visited = localStorage.getItem('visited');
+var visited = sessionStorage.getItem('visited');
 if (!visited) {
   alert("Disclaimer: The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation.");
-  localStorage.setItem('visited', true);
+  sessionStorage.setItem('visited', true);
 }
 
 /* Back to top button */
