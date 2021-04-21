4b235bd69fde661e67423103ef5bc36328f3a804
zachtay1021 <64520390+zachtay1021@users.noreply.github.com>
2021-04-21 16:14:55 -0400

    Update js.js

--- js.js
+++ js.js
@@ -19,10 +19,12 @@ function myFunction() {
 }
 
 /* Alert on visit */
-var visited = sessionStorage.getItem('visited');
-if (!visited) {
-  alert("Disclaimer: The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation.");
-  sessionStorage.setItem('visited', true);
+function welcome() {
+    var visited = sessionStorage.getItem('visited');
+    if (!visited) {
+      alert("Disclaimer: The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation.");
+      sessionStorage.setItem('visited', true);
+    }
 }
 
 /* Back to top button */
